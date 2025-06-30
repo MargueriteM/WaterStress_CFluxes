cbf550a78a70d9c9f972a6363f2b67eda15787ea
mbeckage2 <maegan.beckage@gmail.com>
2025-06-30 12:10:55 -0600

    filtered Reco and GPP data by removing the uncertain 1s and 2s, done for both daytime and night time data.

--- 2_ReddyProc_OnlineTool_Out.R
+++ 2_ReddyProc_OnlineTool_Out.R
@@ -9,12 +9,30 @@ library(viridis)
 setwd("C:/Users/mebeckage/OneDrive - The University of Texas at El Paso/Mauritz Lab - ONAQ_Data")
 
 # import
-rp.units <- (fread("./REddyResults_ONAQ_2024_20250625_942226779/output.txt",
+rp.units <- (fread("./REddyResults_ONAQ_2019_20250625_293430638/output.txt",
                    header=TRUE))[1,]
 
-flux.rp <- fread("./REddyResults_ONAQ_2024_20250625_942226779/output.txt",
+flux.rp2019 <- fread("./REddyResults_ONAQ_2019_20250625_293430638/output.txt",
                  header=FALSE, skip=2,na.strings=c("-9999", "NA","-"),
                  col.names = colnames(rp.units))
+
+flux.rp2020 <- fread("./REddyResults_ONAQ_2020_20250625_92489179/output.txt",
+                     header=FALSE, skip=2,na.strings=c("-9999", "NA","-"),
+                     col.names = colnames(rp.units))
+
+flux.rp2021 <- fread("./REddyResults_ONAQ_2021_20250625_196531586/output.txt",
+                     header=FALSE, skip=2,na.strings=c("-9999", "NA","-"),
+                     col.names = colnames(rp.units))
+
+flux.rp2023 <- fread("./REddyResults_ONAQ_2023_20250625_831786507/output.txt",
+                     header=FALSE, skip=2,na.strings=c("-9999", "NA","-"),
+                     col.names = colnames(rp.units))
+
+flux.rp2024 <- fread("./REddyResults_ONAQ_2024_20250625_942226779/output.txt",
+                     header=FALSE, skip=2,na.strings=c("-9999", "NA","-"),
+                     col.names = colnames(rp.units))
+
+flux.rp <- rbind(flux.rp2019,flux.rp2020, flux.rp2021, flux.rp2023, flux.rp2024)
 
 # for some reason na.strings won't recognize the -9999
 flux.rp[flux.rp == -9999] <- NA
@@ -36,31 +54,116 @@ fig_nee
 fig_nee_fill <- ggplot((flux.rp))+
   geom_line(aes(DoY,NEE_U50_f))+
   theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
+        axis.text.x = element_blank()) +
+  facet_grid(.~Year)
 fig_nee_fill
 
 # graph Ustar filtered Reco with 50th percentile and gap-filled
-fig_reco <- ggplot(subset(flux.rp), aes(DoY,Reco_U50))+geom_line()+
-  geom_line(aes(y=Reco_DT_U50),colour="blue")+
+fig_reco <- ggplot(subset(flux.rp), aes(DoY,Reco_U50, colour = "nightime"))+geom_line()+
+  geom_line(aes(y=Reco_DT_U50, colour = "daytime"))+
   theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
+        axis.text.x = element_blank()) +
+  facet_grid(.~Year)
 fig_reco
+
 
 # plot daytime Reco with qc code (not sure what to do with this code: exlude 1?)
 ggplot(subset(flux.rp), aes(DoY,Reco_DT_U50, colour=factor(FP_qc)))+
-  geom_point(size=1)
+  geom_point(size=1)+
+  facet_grid(.~Year)
+
+# plot daytime Rec, exclude the 2s
+ggplot(subset(flux.rp, FP_qc != 2), aes(DoY, Reco_DT_U50, colour=factor(FP_qc))) +
+  geom_point(size=1) +
+  facet_grid(. ~ Year)
+
+# plot daytime Rec, exclude the 1s and 2s
+ggplot(subset(flux.rp, FP_qc == 0), aes(DoY, Reco_DT_U50, colour=factor(FP_qc))) +
+  geom_point(size=1) +
+  facet_grid(. ~ Year)
+
+########################################################################
+
+# plot nighttime Reco with qc code (not sure what to do with this code: exlude 1?)
+ggplot(subset(flux.rp), aes(DoY,Reco_U50, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year)
+
+# plot nighttime Rec, exclude the 2s
+ggplot(subset(flux.rp, FP_qc != 2), aes(DoY, Reco_U50, colour=factor(FP_qc))) +
+  geom_point(size=1) +
+  facet_grid(. ~ Year)
+
+# plot nighttime Rec, exclude the 1s and 2s
+ggplot(subset(flux.rp, FP_qc == 0), aes(DoY, Reco_U50, colour=factor(FP_qc))) +
+  geom_point(size=1) +
+  facet_grid(. ~ Year)
+
+
+
+
+
 
 # graph Ustar filtered GPP Day time and night time with 50th percentile and gap-filled
-fig_gpp <- ggplot(flux.rp, aes(DoY,GPP_U50_f))+
+fig_gpp <- ggplot(flux.rp, aes(DoY,GPP_U50_f, colour = "nightime"))+
   geom_line()+
-  geom_line(aes(y=GPP_DT_U50),colour="blue")+
+  geom_line(aes(y=GPP_DT_U50, colour = "daytime"))+
   theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank())
-fig_gpp
+        axis.text.x = element_blank()) + 
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+fig_gpp 
+
+
+#  GPP Daytime 50th percentile 
+ggplot(subset(flux.rp), aes(DoY,GPP_DT_U50, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
+# GPP Daytime 50th percentile with various uncertain data removed 
+
+# Day time GPP with 2s removed
+ggplot(subset(flux.rp,FP_qc != 2), aes(DoY,GPP_DT_U50, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
+
+# Day time GPP with 1s and 2s removed
+ggplot(subset(flux.rp,FP_qc == 0), aes(DoY,GPP_DT_U50, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
+###########################################################################
+
+#  GPP nighttime 50th percentile 
+ggplot(subset(flux.rp), aes(DoY,GPP_U50_f, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
+# GPP nighttime 50th percentile with various uncertain data removed 
+
+# nighttime GPP with 2s removed
+ggplot(subset(flux.rp,FP_qc != 2), aes(DoY,GPP_U50_f, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
+
+#  nighttime GPP with 1s and 2s removed
+ggplot(subset(flux.rp,FP_qc == 0), aes(DoY,GPP_U50_f, colour=factor(FP_qc)))+
+  geom_point(size=1)+
+  facet_grid(.~Year) + 
+  ylim(c(ymin = -100, ymax = 100))
+
 
 
 fig_le <- ggplot((flux.rp))+
-  geom_line(aes(DoY,LE_f))
+  geom_line(aes(DoY,LE_f)) +
+  facet_grid(.~Year)
 fig_le
 
 plot_grid(fig_nee_fill, fig_reco, fig_gpp, fig_le, nrow=4, align="v")
